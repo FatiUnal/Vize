1976c5ecf41903355b296bcd4a6161ee1d398874
Fatih <fatih_0226@outlook.com>
2023-11-16 20:52:35 +0300

    1.1,testleri ile tamamlandı

---
+++ 200401011/Labex1_Q1_200401011_unal_fatih.R
@@ -0,0 +1,2 @@
+url <- "https://raw.githubusercontent.com/scizmeli/Red/master/MapsThatChangedOurWorld_StoryMap_Data.csv"
+download(url, destfile = "200401011/MapsThatChangedOurWorld_StoryMap_Data.csv", mode = "wb")
---
+++ 200401011/Labex1_Q1_tests_200401011_unal_fatih.R
@@ -0,0 +1,30 @@
+
+test_that("tüm değişkenleri sil", {
+  rm(list = ls())
+  expect_equal(length(ls()), 0)
+})
+
+
+
+
+test_that("Eğer mevcut ise veri setini silme", {
+  if ("MapsThatChangedOurWorld_StoryMap_Data.csv" %in% list.files()) {
+    file.remove("MapsThatChangedOurWorld_StoryMap_Data.csv")
+  }
+  expect_equal("MapsThatChangedOurWorld_StoryMap_Data.csv" %in% list.files(), FALSE)
+})
+
+
+
+
+test_that("MapsThatChangedOurWorld_StoryMap_Data.csv adlı dosya mevcuttur.", {
+  directory <- getwd()
+  relative_path <- file.path(directory,"/200401011/Labex1_Q1_200401011_unal_fatih.R")
+  source(relative_path)
+  
+  file_path <- file.path(directory,"/200401011/MapsThatChangedOurWorld_StoryMap_Data.csv")
+  expect_true(file.exists(file_path), info = "Dosya yok.")
+  
+})
+
+
